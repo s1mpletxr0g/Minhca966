743a30f9c130952cb07c2ec78c9b5dcbc74e0bf7
s1mpletxr0g <qiangway929963@yeah.net>
2021-06-02 05:42:57 +0800

    o

--- src/ss1_Introduction_to_Java/thuc_hanh/TinhChiSoCanNang.java
+++ src/ss1_Introduction_to_Java/thuc_hanh/TinhChiSoCanNang.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class TinhChiSoCanNang {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double weight, height, bmi;
-        System.out.print("Your weight (in kilogram):");
-        weight = scanner.nextDouble();
+       float weight;
+       float high;
+       double bmi;
+       Scanner scanner=new Scanner(System.in);
+        System.out.println("moi nhap can nang");
+       weight=scanner.nextFloat();
+        System.out.println("moi nhap chieu cao");
+        high=scanner.nextFloat();
+        bmi=weight/Math.pow(high,2);
+        if(bmi<18.8){
+            System.out.println("underweight");
+        }else if(bmi<25){
+            System.out.println("normal");
+        }else if(bmi<30){
+            System.out.println("overweight");
+        }else{
+            System.out.println("obese");
+        }
 
-        System.out.print("Your height (in meter):");
-        height = scanner.nextDouble();
-        bmi = weight / Math.pow(height, 2);
-        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
-
-        if (bmi < 18)
-            System.out.printf("%-20.2f%s", bmi, "Underweight");
-        else if (bmi < 25.0)
-            System.out.printf("%-20.2f%s", bmi, "Normal");
-        else if (bmi < 30.0)
-            System.out.printf("%-20.2f%s", bmi, "Overweight");
-        else
-            System.out.printf("%-20.2f%s", bmi, "Obese");
     }
 }
