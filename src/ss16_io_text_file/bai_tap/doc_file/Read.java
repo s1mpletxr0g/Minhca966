6d7d7f1a31d420c40cd9fc109053f1ce9440dc4a
s1mpletxr0g <qiangway929963@yeah.net>
2021-08-12 12:34:14 +0800

    j

---
+++ src/ss16_io_text_file/bai_tap/doc_file/Read.java
@@ -0,0 +1,32 @@
+package ss16_io_text_file.bai_tap.doc_file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public class Read {
+    public static void main(String[] args) {
+        File file = new File("D:\\up_git\\ca\\src\\ss16_io_text_file\\bai_tap\\doc_file\\country.csv");
+
+
+        try {
+            BufferedReader rd = Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8);
+            String line = null;
+            while (true) {
+                line = rd.readLine();
+                if (line==null){
+                    break;
+                }else{
+                    System.out.println(line);
+                }
+            } }
+        catch(IOException e){
+                e.printStackTrace();
+            }
+
+
+
+    }
+}
